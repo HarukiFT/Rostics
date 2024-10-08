ea6d7fdb44fd5cd329f93a030c47806c4ad45734
Haruki <aratashiharuki@gmail.com>
2024-10-08 18:00:44 +0300

    Add cached

--- backend/src/modules/likes/likes.module.ts
+++ backend/src/modules/likes/likes.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { LikesController } from './likes.controller';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
+  imports: [CacheModule.register()],
   providers: [LikesService],
-  controllers: [LikesController]
+  controllers: [LikesController],
 })
 export class LikesModule {}
--- backend/src/modules/likes/likes.service.ts
+++ backend/src/modules/likes/likes.service.ts
@@ -1,13 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class LikesService {
+  constructor(@Inject('CACHE_MANAGER') private readonly cacheManager: Cache) {}
+
   async getLikes(universeId: string) {
-    return (
+    const cached = await this.cacheManager.get(universeId);
+    if (cached) {
+      return cached;
+    }
+
+    const data = (
       await axios.get(
         `https://games.roblox.com/v1/games/votes?universeIds=${universeId}`,
       )
     ).data.data[0];
+
+    this.cacheManager.set(universeId, data, 30000);
+
+    return data;
   }
 }
